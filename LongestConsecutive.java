ffb8952a2db5762b1ee0396bdd4103f47922c0dc
yichiii <55825777+yichii@users.noreply.github.com>
2025-04-23 23:52:40 -0700

    Still not done

---
+++ LongestConsecutive.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class LongestConsecutive {
+    public int longestConsecutive(int[] nums) {
+        int n = 0;
+        ArrayList<> numbers = new ArrayList<>(Arrays.asList(nums));
+        for (int num : nums) {
+            min = Arrays.stream(nums).min().getAsInt();
+        }
+        return n;
+    }
+}
--- Main.java
+++ Main.java
@@ -2,9 +2,8 @@ import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args) {
-        int[] nums = {1,1,1,2,2,2,2,3};
-        int k = 2;
-        TopKFrequent tkf = new TopKFrequent();
-        System.out.println(Arrays.toString(tkf.topKFrequent(nums, k)));
+        int[] nums = {0,3,7,2,5,8,4,6,0,1};
+        LongestConsecutive lc = new LongestConsecutive();
+        System.out.println(lc.longestConsecutive(nums));
     }
 }
